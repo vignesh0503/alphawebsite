36ffbc2ad560feb9babbd107d489cb8fc049aa93
vignesh0503 <vignesh.s170503@gmail.com>
2025-01-23 15:25:39 +0530

    changes in navbar

--- src/NavBarSection.jsx
+++ src/NavBarSection.jsx
@@ -1,31 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoimg from "./assets/images/Logo.png";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBarSection = () => {
   return (
     <div className="navbar">
       {/* <div className="nav"> */}
-        <div className="logo">
-          <img src={logoimg} alt="Logo-img" />
-        </div>
+      <div className="logo">
+        <img src={logoimg} alt="Logo-img" />
+      </div>
 
-        <div className="navbar-content">
-          <input type="checkbox" id="check" />
-          <label for="checkbtn" className="checkbtn">
-            <FaBars className="bar-icon" />
-          </label>
-          <ul className="nav-ul">
-            <li>
-              <Link to={"/"}>Home</Link>
-              <Link to={""}>About</Link>
-              <Link to={""}>Services</Link>
-              <Link to={""}>Courses</Link>
-              <Link to={""}>Contact</Link>
-            </li>
-          </ul>
-        </div>
+      <div className="navbar-content">
+        <input type="checkbox" id="check" />
+
+        <label htmlFor="check" className="checkbtn">
+          <FaBars className="bar-icon" />
+          <FaTimes className="x-icon" />
+        </label>
+
+        <ul className="nav-ul">
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/services"}>Services</Link>
+          </li>
+          <li>
+            <Link to={"/courses"}>Courses</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
       {/* </div> */}
     </div>
   );
